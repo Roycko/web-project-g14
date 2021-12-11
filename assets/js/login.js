5aa52b7c7033e615a0246517d253adee601e5fb8
orimilbauer <orimilbauer@gmail.com>
2021-12-11 14:25:44 +0200

    Ori changes
    Menu+signup+login popup
    needed checks: how to change qty box width, why the clicks on person doesn't always works,  and the resonsive with the all min icons ?

---
+++ assets/js/login.js
@@ -0,0 +1,9 @@
+
+function open_login(){
+    console.log('hi');
+    document.querySelector(".popup").style.display="flex";
+}
+function close_login(){
+    console.log('close');
+    document.querySelector(".popup").style.display="none";
+}
