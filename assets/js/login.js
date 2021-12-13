e9cd2f7beeb85c39c90c22c013f8d8f48a5487bc
Roy Nitzan <Roy.N@Solaredge.com>
2021-12-13 19:58:55 +0200

    Clean Code - Roy

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,7 @@
 
 function open_login(){
-    console.log('hi');
     document.querySelector(".popup").style.display="flex";
 }
 function close_login(){
-    console.log('close');
     document.querySelector(".popup").style.display="none";
 }
